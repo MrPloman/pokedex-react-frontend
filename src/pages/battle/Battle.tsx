b47aa35d65b8aa213eed81f8716e7b7eeee68b2e
MrPloman <polplanaayet@gmail.com>
2023-12-19 20:40:44 -0600

    background fix

--- src/pages/battle/Battle.tsx
+++ src/pages/battle/Battle.tsx
@@ -5,6 +5,7 @@ import { UseLogicGame } from "../../hooks/UseLogicGame";
 import "./Battle.scss";
 import { useNavigate } from "react-router-dom";
 import { setLoading } from "../../store/slices/battle.slice";
+import ProgressBar from "react-bootstrap/ProgressBar";
 
 export const Battle = () => {
     const { player2, player1, loading } = useSelector((state: any) => state.battleStore);
@@ -23,20 +24,32 @@ export const Battle = () => {
 
     return (
         <>
-            <div>
+            <div id="page">
                 <Navbar />
                 <div id="playground">
                     {!!loading ? (
                         <div>...LOADING...</div>
                     ) : (
-                        <div id="battle">
-                            <div className="playerSide" id="playerSide1">
-                                <img src={player1.pokemons[0].sprites.back_default} />
+                        <>
+                            <div id="battle">
+                                <div className="playerSide" id="playerSide1">
+                                    <div className="pokemonStatus" id="pokemon2Status">
+                                        <div>{player2.pokemons[0].name}</div>
+
+                                        <ProgressBar className="hpBar" now={60} variant="danger" />
+                                    </div>
+                                    <img src={player1.pokemons[0].sprites.back_default} />
+                                </div>
+                                <div className="playerSide" id="playerSide2">
+                                    <img src={player2.pokemons[0].sprites.front_default} />
+                                    <div className="pokemonStatus" id="pokemon1Status">
+                                        <div>{player1.pokemons[0].name}</div>
+                                        <ProgressBar className="hpBar" now={60} variant="danger" />
+                                    </div>
+                                </div>
                             </div>
-                            <div className="playerSide" id="playerSide2">
-                                <img src={player2.pokemons[0].sprites.front_default} />
-                            </div>
-                        </div>
+                            <div id="textSection"></div>
+                        </>
                     )}
                 </div>
             </div>
